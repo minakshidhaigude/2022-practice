3b96fe898eca6940f507ef698c0262dcfef5f103
minakshidhaigude <dhaigudeminakshi@gmail.com>
2022-12-02 16:22:58 +0530

    latest2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MyappsComponent } from './myapps/myapps.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { TodonotesComponent } from './todonotes/todonotes.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    MyappsComponent,
+    CalculatorComponent,
+    TodonotesComponent,
+    CurrencyComponent,
+    TictactoeComponent,
+    GameComponent,
+    BoardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
